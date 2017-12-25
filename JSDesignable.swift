7117e788653bc810f917df5cfd96ed61bdc22cbc
imjog <imjog24@gmail.com>
2017-12-25 14:34:15 +0530

    JSButton and JSLabel added

--- JSDesignable.swift
+++ JSDesignable.swift
@@ -6,8 +6,56 @@
 //
 import UIKit
 
+// MARK: JSTextField
+
 @IBDesignable
 public class JSTextField: UITextField {
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRedius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRedius
+        }
+    }
+}
+
+// MARK: JSButton
+
+@IBDesignable
+public class JSButton: UIButton {
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRedius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRedius
+        }
+    }
+}
+
+// MARK: JSLabel
+
+@IBDesignable
+public class JSLabel: UILabel {
     @IBInspectable var borderWidth: CGFloat = 0.0 {
         didSet {
             self.layer.borderWidth = borderWidth
